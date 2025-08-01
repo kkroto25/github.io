4b72dac1edcf2261cdbd13aeee555e3b26e9fc98
kkroto25 <36181079+kkroto25@users.noreply.github.com>
2025-08-01 01:33:22 -0500

    Update config.js 02

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // config.js
-const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby7Dl-hxZ_CzQ-wlE67tVAJz9Ahu9rlzEQWgSl8w6vgq_rlvVMIMKVV85kwdC3UFf3a/exec';
+const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbze7haRtQsXtsR8PBeheDCXgEjkkv61c8GFYYVw4LBh2RfJHV-Zq0QRvdsh1vhalq_W/exec';
