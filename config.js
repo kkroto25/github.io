523154752fc835a3bb5eade55e551800eecdd6f3
kkroto25 <36181079+kkroto25@users.noreply.github.com>
2025-08-01 20:30:49 -0500

    test12-04

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 // config.js
 const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxweJ7KVj1t8QYKJPZaVJWnJwEvSrY6VkNJqRPo-U8SgByPFT3f7zfXk0yJ47XM7eyy/exec';
-const monthNames = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']; 
+const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
 
     
