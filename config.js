e3e9035b33d2d9a0c93ec0851e2051b7ed365305
kkroto25 <36181079+kkroto25@users.noreply.github.com>
2025-08-02 19:21:12 -0500

    test13-02

--- config.js
+++ config.js
@@ -2,6 +2,10 @@
 const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzGqLjWneYM13BQjllPpDW4wH0qkyoma5hQKi985shOsoB28y0H1SMRL37yoIN8A9uX/exec';
 const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
+// Nombres de tus hojas de cálculo
+const HOJA_DETALLE = 'Datos'; // Nombre de tu hoja de registros principal
+const HOJA_TOTALES = 'Totales'; // Nombre de tu hoja de totales
+
 
 
     
